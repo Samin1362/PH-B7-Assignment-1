2bf6a60c98f21cc79d85afbfdf8cd9d99905a60e
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2026-05-07 19:33:23 +0600

    initial commit

---
+++ solutions.ts
@@ -0,0 +1,89 @@
+// Batch 7 - Assignment 1 - Md. Samin Israk (saminisrak1991@gmail.com)
+
+// problem 1
+function filterEvenNumbers(arr: number[]): number[] {
+  const result = arr.filter((index) => index % 2 === 0)
+  return result;
+}
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// problem 2
+
+function reverseString(str: string): string {
+  const reverse = str.split('').reverse().join('');
+  return reverse;
+}
+
+console.log(reverseString("typescript"));
+
+// problem 3
+
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): string {
+  if (typeof value === "string"){
+    return "String";
+  } else{
+    return "Number";
+  }
+}
+
+console.log(checkType(54))
+
+// problem 4
+
+function getProperty <T, K extends keyof T> (obj: T, key: K): T[K]{
+  return obj[key];
+}
+
+const user = { id: 1, name: "John Doe", age: 21 };
+console.log(getProperty(user, "name"));
+
+// problem 5 
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number
+}
+
+function toggleReadStatus (book: Book): Book & {isRead: boolean} {
+  return {...book, isRead: true};
+}
+
+const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+console.log(toggleReadStatus(myBook));
+
+// problem 6 
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor (name: string, age: number){
+    this.name = name;
+    this.age  = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+  
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  //"Name: Alice, Age: 20, Grade: A";
+  getDetails() : string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails());
+
+// problem 7
+
+
